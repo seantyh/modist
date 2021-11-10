2bef55b8c49595e71643f018c03dad89f257b62c
seantyh <seantyh@gmail.com>
2021-11-10 13:11:11 +0800

    shuffle mp3 list when randomize_seg

--- src/modist/modist_data_utils.py
+++ src/modist/modist_data_utils.py
@@ -6,7 +6,7 @@ import pydub
 from .mp3_info import get_lang, get_segment_type, get_segments
 
 class ModistDataset(IterableDataset):
-    def __init__(self, w2v_feat_extractor, mp3_list, 
+    def __init__(self, w2v_feat_extractor, mp3_list,
                 sample_rate=16000, batch_size=16, secs_per_seq=5,
                 speech_only=False, randomize_seg=False, random_seed=None):
         self.feature_extractor = w2v_feat_extractor
@@ -16,21 +16,24 @@ class ModistDataset(IterableDataset):
         self.secs_per_seq = secs_per_seq
         self.speech_only = speech_only
         self.randomize_seg = randomize_seg
-        self.random_seed = random_seed     
+        self.random_seed = random_seed
 
     def __iter__(self):
-        mp3_list = self.mp3_list
+        mp3_list = self.mp3_list[::1]
+        if self.randomize_seg:
+            random.seed(self.random_seed)
+            random.shuffle(mp3_list)
         mp3_iter = map(self.make_mp3_tuple, mp3_list)
         seg_iter = starmap(self.make_sample_tuple, mp3_iter)
         batch_iter = map(lambda seg: self.batch(seg, self.batch_size), seg_iter)
-        data_iter = chain.from_iterable(batch_iter)             
+        data_iter = chain.from_iterable(batch_iter)
         return data_iter
 
     def make_mp3_tuple(self, mp3_name):
         mp3_dub = pydub.AudioSegment.from_mp3(mp3_name)
         mp3_lang = get_lang(mp3_name)
         return (mp3_name, mp3_dub, mp3_lang)
-    
+
     def make_sample_tuple(self, mp3_name, dub, lang):
         seg_iter = self.segment(mp3_name, dub, self.secs_per_seq)
         return ((lang, category, samples)
@@ -39,15 +42,15 @@ class ModistDataset(IterableDataset):
 
     def segment(self, mp3_name, mp3_dub, secs=5):
         blen = secs * 1000
-        
+
         offsets = list(range(0, len(mp3_dub), blen))
         if self.randomize_seg:
             random.seed(self.random_seed)
-            random.shuffle(offsets)        
+            random.shuffle(offsets)
 
         for offset in offsets:
             # category: (category, matched_secs)
-            category = get_segment_type(mp3_name, int(offset/1000), int((offset+blen)/1000))            
+            category = get_segment_type(mp3_name, int(offset/1000), int((offset+blen)/1000))
             if category[1] != secs:
                 continue
             # replace category with category name only
@@ -55,19 +58,19 @@ class ModistDataset(IterableDataset):
             if self.speech_only and category != "speech":
                 continue
 
-            samples = np.array(mp3_dub[offset:offset+blen].get_array_of_samples(), 
-                                dtype=np.double)            
+            samples = np.array(mp3_dub[offset:offset+blen].get_array_of_samples(),
+                                dtype=np.double)
             yield (category, samples)
 
-    def batch(self, seg_iter, batch_size):  
+    def batch(self, seg_iter, batch_size):
         batch_iter = iter(lambda: list(islice(seg_iter, batch_size)), [])
         feature_extractor = self.feature_extractor
         sample_rate = self.sample_rate
-        for i, batched_data in enumerate(batch_iter):            
-            langs, categories, samples = list(zip(*batched_data))            
+        for i, batched_data in enumerate(batch_iter):
+            langs, categories, samples = list(zip(*batched_data))
             if len(samples) < batch_size: continue
 
             in_tensor = feature_extractor(
                 samples, sampling_rate=sample_rate, padding=True,
-                return_tensors="pt").input_values            
+                return_tensors="pt").input_values
             yield {"tensorX": in_tensor, "lang": langs, "category": categories}
